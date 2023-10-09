c4692d12ee019f6689b7b5b88631eec47b9d3035
WhaiLnwza008 <110302110+WhaiLnwza008@users.noreply.github.com>
2023-10-09 21:07:23 +0700

    Add files via upload

---
+++ Travel-portfolio/index.js
@@ -0,0 +1,64 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-nav-toggle');
+
+navBarToggle.addEventListener("click", function(){
+    mainNav.classList.toggle('active');
+})
+
+let popup =document.getElementById("popup")
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+let popup2 =document.getElementById("popup2")
+
+function openPopup2() {
+    popup2.classList.add("open-popup2");
+}
+function closePopup2() {
+    popup2.classList.remove("open-popup2");
+}
+
+let popup3 =document.getElementById("popup3")
+
+function openPopup3() {
+    popup3.classList.add("open-popup3");
+}
+function closePopup3() {
+    popup3.classList.remove("open-popup3");
+}
+
+let popup4 =document.getElementById("popup4")
+
+function openPopup4() {
+    popup4.classList.add("open-popup4");
+}
+function closePopup4() {
+    popup4.classList.remove("open-popup4");
+}
+
+let popup5 =document.getElementById("popup5")
+
+function openPopup5() {
+    popup5.classList.add("open-popup5");
+}
+function closePopup5() {
+    popup5.classList.remove("open-popup5");
+}
+
+let popup6 =document.getElementById("popup6")
+
+function openPopup6() {
+    popup6.classList.add("open-popup6");
+}
+function closePopup6() {
+    popup6.classList.remove("open-popup6");
+}
+
+
+
+
